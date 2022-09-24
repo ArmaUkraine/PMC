99fb224287d7b76ae644ad405d9d097a4e7bda84
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2022-09-24 08:51:37 +0300

    refactor, add server start scripts

---
+++ run_server.py
@@ -0,0 +1,19 @@
+import os
+import settings
+
+os.chdir(settings.SERVER_PATH)
+os.system("taskkill /F /IM arma3server_x64.exe /T")
+cmd = " ".join(
+    [
+        "arma3server_x64.exe",
+        "-filePatching",
+        "-autoInit",
+        "-debug",
+        f'-profiles="{settings.SERVER_PROFILE_PATH}"',
+        f'-config="{settings.SERVER_SETTINGS_PATH}"',
+        f"-mod=\"{';'.join(settings.MODS)}\"",
+    ]
+)
+
+print(cmd)
+os.system(cmd)
---
+++ settings.py
@@ -0,0 +1,24 @@
+import pathlib
+from bs4 import BeautifulSoup
+import pprint
+
+PROJECT_PATH = pathlib.Path(".").absolute()
+CLIENT_PATH = pathlib.Path("D:\\SteamLibrary\\steamapps\\common\\Arma 3")
+TOOLS_PATH = pathlib.Path("D:\\SteamLibrary\\steamapps\\common\\Arma 3 Tools")
+SERVER_PATH = pathlib.Path("D:\\SteamLibrary\\steamapps\\common\\Arma 3 Server")
+SERVER_PROFILE_PATH = pathlib.Path(PROJECT_PATH / "server_profile")
+SERVER_SETTINGS_PATH = pathlib.Path(PROJECT_PATH / "server.cfg")
+
+with open("preset.html") as fh:
+    # Parse preset.
+    soup = BeautifulSoup(fh, "html.parser")
+    mod_names = []
+    for tr in soup.find_all("tr"):
+        mod_name = tr.find_all("td")[0].text
+        mod_names.append(mod_name)
+
+    # Copy keys.
+
+    # Prepare list of mods.
+    MODS = [f"p:\\workshop\@{x}" for x in mod_names]
+    pprint.pprint(MODS)
