7ea81ab4b292ea7f9c0ac6c1cd2a79ce8c46a285
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2022-09-24 09:54:16 +0300

    automate mods loading through ids rather then names

--- settings.py
+++ settings.py
@@ -8,15 +8,16 @@ TOOLS_PATH = pathlib.Path("D:\\SteamLibrary\\steamapps\\common\\Arma 3 Tools")
 SERVER_PATH = pathlib.Path("D:\\SteamLibrary\\steamapps\\common\\Arma 3 Server")
 SERVER_PROFILE_PATH = pathlib.Path(PROJECT_PATH / "server_profile")
 SERVER_SETTINGS_PATH = pathlib.Path(PROJECT_PATH / "server.cfg")
+WORKSHOP_PATH = pathlib.Path("E:\\SteamLibrary\\steamapps\\workshop\\content\\107410")
 
 with open("preset.html") as fh:
     # Parse preset.
     soup = BeautifulSoup(fh, "html.parser")
-    mod_names = []
-    for tr in soup.find_all("tr"):
-        mod_name = tr.find_all("td")[0].text
-        mod_names.append(mod_name)
+    mod_ids = []
+    for anchor in soup.find_all("a"):
+        mod_id = anchor.attrs["href"].split("=")[-1]
+        mod_ids.append(mod_id)
 
     # Prepare list of mods.
-    MODS = [f"p:\\workshop\@{x}" for x in mod_names]
+    MODS = [f"{WORKSHOP_PATH}/{x}" for x in mod_ids]
     pprint.pprint(MODS)
